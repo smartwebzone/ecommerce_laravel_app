01fe3a648e79a08f89aa1561239f8516e4f22b5a
Ravi Verma <Ravi Verma@192.168.1.7>
2017-10-12 22:13:08 +0530

    Standard

--- resources/views/backend/standard/fields.blade.php
+++ resources/views/backend/standard/fields.blade.php
@@ -9,7 +9,7 @@
 
 <!-- Phone Field -->
 <div class="form-group col-sm-6">
-    {!! Form::label('description', 'Phone:') !!}
+    {!! Form::label('description', 'Description:') !!}
     {!! Form::text('description', null, ['class' => 'form-control','value' => old('description'),'required' => true]) !!}
 </div>
 
