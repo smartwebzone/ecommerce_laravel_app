f74e83ae5a7065b20f75a5b9781fdfa8cfcf37d8
Rohit <Rohit@192.168.1.24>
2017-11-24 16:09:20 +0530

    Jeevandeep changes/fixes

--- resources/views/frontend/layout/jeevandeep/header.blade.php
+++ resources/views/frontend/layout/jeevandeep/header.blade.php
@@ -5,6 +5,7 @@ $cart_count = getCartCount();
 <h2><i class="fa fa-book"></i>Online Store
     @if(Sentinel::check() && $cart_menu == true)
     <ul class="user-cart">
+        <li><a href="{{url(getLang().'/store/selectSchool')}}"><i class="fa fa-home"></i><span>Home</span></a></li>
         <li><a href="{{url('/my_profile')}}"><i class="fa fa-group"></i><span>My Account</span></a></li>
         <li><a href="{{url('/signout')}}"><i class="fa fa-sign-out"></i><span>Logout</span></a></li>
         <li>
